9c5fd40bf95868c65bab06e9605d65240c5272d7
CodrutBefuAccess <codrut_befu@yahoo.com>
2024-04-11 19:10:12 +0300

    config for local

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Ramsey\Uuid\Type\Integer;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -18,11 +20,18 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('123456'),
             'remember_token' => Str::random(10),
+            'gender' => 'mr',
+            'address' =>  fake()->address(),
+            'city' =>  fake()->city(),
+            'country' =>  fake()->country(),
+            'zip' =>  fake()->postcode(),
+            'about' =>  fake()->text(),
         ];
     }
 
--- database/migrations/2023_11_06_135110_create_club_table.php
+++ database/migrations/2023_11_06_135110_create_club_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('club', function (Blueprint $table) {
+        Schema::create('club', function (Blueprint $table) {
             $table->string('name');
             $table->string('city')->nullable();
             $table->string('country')->nullable();
